06c0c4f93fa95d4cb29320dc1490d5c33ad76501
felipegabrielsantos <felipe.gabriel.santos.miranda@gmail.com>
2026-09-21 18:49:09 -0300

    jogo da forca v01

---
+++ forca/forca/src/forca/main.java
@@ -0,0 +1,183 @@
+package forca;
+import javax.swing.JOptionPane;
+public class main {
+	public static void main(String args []) {
+		int c = Integer.parseInt(JOptionPane.showInputDialog("insira a quantidade de caracteres:")), n;
+		String palavra[] = new String[c], entrada = "";
+		int Npalavra[] = new int[c];
+		for (int i = 1; i <= c; i++) {
+			palavra[i-1] = JOptionPane.showInputDialog("insira a letra da posição " + i);
+			switch (palavra[i]) {
+			case "a":
+				n = 1;
+				break;
+			case "b":
+				n = 2;
+				break;
+			case "c":
+				n = 3;
+				break;
+			case "d":
+				n = 4;
+				break;
+			case "e":
+				n = 5;
+				break;
+			case "f":
+				n = 6;
+				break;
+			case "g":
+				n = 7;
+				break;
+			case "h":
+				n = 8;
+				break;
+			case "i":
+				n = 9;
+				break;
+			case "j":
+				n = 10;
+				break;
+			case "k":
+				n = 11;
+				break;
+			case "l":
+				n = 12;
+				break;
+			case "m":
+				n = 13;
+				break;
+			case "n":
+				n = 14;
+				break;
+			case "o":
+				n = 15;
+				break;
+			case "p":
+				n = 16;
+				break;
+			case "q":
+				n = 17;
+				break;
+			case "r":
+				n = 18;
+				break;
+			case "s":
+				n = 19;
+				break;
+			case "t":
+				n = 20;
+				break;
+			case "u":
+				n = 21;
+				break;
+			case "v":
+				n = 22;
+				break;
+			case "w":
+				n = 23;
+				break;
+			case "x":
+				n = 24;
+				break;
+			case "y":
+				n = 25;
+				break;
+			case "z":
+				n = 26;
+				break;
+			}
+			Npalavra[i] = n;
+		}
+		
+		
+		
+		for (int i = 0; v ) {
+			entrada = JOptionPane.showInputDialog("");
+			switch (entrada) {
+			case "a":
+				n = 1;
+				break;
+			case "b":
+				n = 2;
+				break;
+			case "c":
+				n = 3;
+				break;
+			case "d":
+				n = 4;
+				break;
+			case "e":
+				n = 5;
+				break;
+			case "f":
+				n = 6;
+				break;
+			case "g":
+				n = 7;
+				break;
+			case "h":
+				n = 8;
+				break;
+			case "i":
+				n = 9;
+				break;
+			case "j":
+				n = 10;
+				break;
+			case "k":
+				n = 11;
+				break;
+			case "l":
+				n = 12;
+				break;
+			case "m":
+				n = 13;
+				break;
+			case "n":
+				n = 14;
+				break;
+			case "o":
+				n = 15;
+				break;
+			case "p":
+				n = 16;
+				break;
+			case "q":
+				n = 17;
+				break;
+			case "r":
+				n = 18;
+				break;
+			case "s":
+				n = 19;
+				break;
+			case "t":
+				n = 20;
+				break;
+			case "u":
+				n = 21;
+				break;
+			case "v":
+				n = 22;
+				break;
+			case "w":
+				n = 23;
+				break;
+			case "x":
+				n = 24;
+				break;
+			case "y":
+				n = 25;
+				break;
+			case "z":
+				n = 26;
+				break;
+			}
+			if (Npalavra[i] = n) {
+				
+			}
+		}
+		
+	}
+}
